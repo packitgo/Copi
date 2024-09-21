9b14ce17fdbff14cabc412ddd107e94c88b610bf
packitgo <abestbox@gmail.com>
2024-09-21 21:09:16 +0900

    Update example.py

--- example.py
+++ example.py
@@ -15,36 +15,22 @@ class ExampleSpider(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super(ExampleSpider, self).__init__(*args, **kwargs)
         
+        # Настройка логирования
         self.site_name = kwargs.get('site_name', 'default_site')
         self.category = kwargs.get('category', 'default_category')
         today = datetime.now().strftime("%Y-%m-%d")
         formatted_category = self.category.replace(" ", "_")
-        
-        log_dir = os.path.join('C:\\Users\\inyur\\ScraperProject\\logs')
-        log_file = os.path.join(log_dir, f'{self.site_name}_{formatted_category}_{today}.log')
-        
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-        
+        log_file = os.path.join('C:/Users/inyur/ScraperProject/logs', f'{self.site_name}_{formatted_category}_{today}.log')
         logging.basicConfig(filename=log_file, level=logging.DEBUG, 
                             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.DEBUG)
-        console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levellevel)s - %(message)s'))
+        console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
         self.my_logger = logging.getLogger(__name__)
         self.my_logger.addHandler(console_handler)
 
-        driver_path = os.path.join('C:\\Users\\inyur\\Favorites\\geckodriver.exe')
-        
-        if not os.path.exists(driver_path):
-            self.my_logger.error(f"Geckodriver not found at {driver_path}")
-            raise FileNotFoundError(f"Geckodriver not found at {driver_path}")
-        
-        self.my_logger.info(f"Using geckodriver at: {driver_path}")
-        self.driver = webdriver.Firefox(service=Service(driver_path))
-        
-        results_base_dir = os.path.join('C:\\Users\\inyur\\ScraperProject\\results')
-        self.results_dir = os.path.join(results_base_dir, self.site_name, formatted_category)
+        self.driver = webdriver.Firefox(service=Service('C:/Users/inyur/Favorites/geckodriver.exe'))
+        self.results_dir = os.path.join('C:/Users/inyur/ScraperProject/results', self.site_name, formatted_category)
         if not os.path.exists(self.results_dir):
             os.makedirs(self.results_dir)
             self.my_logger.info(f"Created results directory: {self.results_dir}")
@@ -55,19 +41,18 @@ class ExampleSpider(scrapy.Spider):
         self.my_logger.info(f"Excel file will be saved at: {self.excel_file}")
         self.init_excel()
 
-        self.delay = 10
-        self.max_delay = 30
-        self.delay_step = 2
+        # Динамическая регулировка задержек
+        self.delay = 10  # Начальная задержка в секундах
+        self.max_delay = 30  # Максимальная задержка
+        self.delay_step = 2  # Шаг изменения задержки
 
     def init_excel(self):
         if not os.path.exists(self.excel_file):
             try:
                 df = pd.DataFrame(columns=[
-                    'No', 'URL', 'product_name', 'price', 'description', 
-                    'main_image_1', 'main_image_2', 'main_image_3',
-                    'description_images_1', 'description_images_2',
-                    'product_code', 'size', 'tip', 'thickness', 'material', 
-                    'color', 'quantity_in_box', 'URL type'
+                    'No', 'URL', 'product_name', 'price', 'description', 'main_image',
+                    'other_pictures', 'description_images', 'product_code', 'size',
+                    'tip', 'thickness', 'material', 'color', 'quantity_in_box', 'URL type'
                 ])
                 df.to_excel(self.excel_file, index=False)
                 self.my_logger.info(f"Created new Excel file: {self.excel_file}")
@@ -80,7 +65,7 @@ class ExampleSpider(scrapy.Spider):
     def start_requests(self):
         self.my_logger.info("Starting spider requests")
         formatted_category = self.category.replace(" ", "_")
-        links_file = os.path.join('C:\\Users\\inyur\\ScraperProject\\data', self.site_name, formatted_category, 'results.csv')
+        links_file = os.path.join('C:/Users/inyur/ScraperProject/data', self.site_name, formatted_category, 'results.csv')
         self.my_logger.info(f"Checking for links file at: {links_file}")
         if os.path.exists(links_file):
             self.my_logger.info(f"Found links file: {links_file}")
@@ -101,167 +86,129 @@ class ExampleSpider(scrapy.Spider):
         self.my_logger.info(f"Processing URL: {response.url}")
         self.driver.get(response.url)
 
-        time.sleep(self.delay + random.uniform(0, 2))
+        # Проверка на признаки блокировки и регулировка задержки
         if self.is_blocked(response):
             self.delay = min(self.delay + self.delay_step, self.max_delay)
-            self.my_logger.warning(f"Possible blocking, increasing delay to {self.delay} seconds.")
+            self.my_logger.warning(f"Признаки блокировки, увеличение задержки до {self.delay} секунд.")
         else:
             self.delay = max(self.delay - self.delay_step, 1)
-            self.my_logger.debug(f"Decreasing delay to {self.delay} seconds.")
+            self.my_logger.debug(f"Уменьшение задержки до {self.delay} секунд.")
 
+        # Случайная задержка перед следующим запросом
+        time.sleep(self.delay + random.uniform(0, 2))
+
+        # Проверка на загрузку основных элементов
         try:
             self.driver.implicitly_wait(10)
-            product_data = self.extract_product_data()
-            self.update_excel(response.url, product_data, response.meta['url_type'])
+            product_name_element = self.driver.find_element(By.CSS_SELECTOR, 'div.options')
         except Exception as e:
-            self.my_logger.error(f"Error processing URL {response.url}: {e}")
+            self.my_logger.error(f"Main elements not loaded for URL: {response.url} - {e}")
+            return
 
-    def extract_product_data(self):
-        product_datafields = [
-            ('product_name', self.get_product_name),
-            ('price', self.get_price),
-            ('description', self.get_description),
-            ('main_image_1', self.get_main_image_1),
-            ('main_image_2', self.get_main_image_2),
-            ('main_image_3', self.get_main_image_3),
-            ('description_images_1', self.get_description_images_1),
-            ('description_images_2', self.get_description_images_2),
-            ('product_code', self.get_product_code),
-            ('size', self.get_size),
-            ('tip', self.get_tip),
-            ('thickness', self.get_thickness),
-            ('material', self.get_material),
-            ('color', self.get_color),
-            ('quantity_in_box', self.get_quantity_in_box)
-        ]
+        # Извлечение данных
+        product_name = price = description = main_image = product_code = size = tip = thickness = material = color = quantity_in_box = None
+        other_pictures = description_images = []
 
-        product_data = {}
-        for field, method in product_datafields:
-            try:
-                product_data[field] = method()
-            except Exception as e:
-                self.my_logger.error(f"Error extracting {field}: {e}")
-                product_data[field] = None
-
-        return product_data
-
-    def get_product_name(self):
         try:
-            return self.driver.find_element(By.CSS_SELECTOR, 'div.product-name').text.strip()
+            product_name = product_name_element.text.strip()
         except Exception as e:
-            self.my_logger.error(f"Product name not found: {e}")
-            return None
+            self.my_logger.error(f"Error extracting product name: {e}")
 
-    def get_price(self):
         try:
-            return self.driver.find_element(By.CSS_SELECTOR, 'span.price').text.strip()
+            price_element = self.driver.find_element(By.CSS_SELECTOR, 'span.p.opensans')
+            price = price_element.text.strip()
         except Exception as e:
-            self.my_logger.error(f"Price not found: {e}")
-            return None
+            self.my_logger.error(f"Error extracting price: {e}")
 
-    def get_description(self):
         try:
-            return self.driver.find_element(By.CSS_SELECTOR, 'div.description').text.strip()
+            description_element = self.driver.find_element(By.CSS_SELECTOR, 'div.product_content_desc')
+            description = description_element.text.strip()
         except Exception as e:
-            self.my_logger.error(f"Description not found: {e}")
-            return None
+            self.my_logger.error(f"Error extracting description: {e}")
 
-    def get_main_image_1(self):
         try:
-            return self.driver.find_element(By.CSS_SELECTOR, 'img.main-image').get_attribute('src')
+            main_image_element = self.driver.find_element(By.CSS_SELECTOR, 'img[src*="/shopimg_new/"]')
+            main_image = main_image_element.get_attribute('src')
         except Exception as e:
-            self.my_logger.error(f"Main image 1 not found: {e}")
-            return None
+            self.my_logger.error(f"Error extracting main image: {e}")
 
-    def get_main_image_2(self):
         try:
-            images = self.driver.find_elements(By.CSS_SELECTOR, 'img.additional-image')
-            return images[0].get_attribute('src') if len(images) > 0 else None
+            other_pictures_elements = self.driver.find_elements(By.CSS_SELECTOR, 'div.swiper-slide img')
+            other_pictures = [img.get_attribute('src') for img in other_pictures_elements]
         except Exception as e:
-            self.my_logger.error(f"Main image 2 not found: {e}")
-            return None
+            self.my_logger.error(f"Error extracting other pictures: {e}")
 
-    def get_main_image_3(self):
         try:
-            images = self.driver.find_elements(By.CSS_SELECTOR, 'img.additional-image')
-            return images[1].get_attribute('src') if len(images) > 1 else None
+            description_images_elements = self.driver.find_elements(By.CSS_SELECTOR, 'div.add_contents img')
+            description_images = [img.get_attribute('src') for img in description_images_elements]
         except Exception as e:
-            self.my_logger.error(f"Main image 3 not found: {e}")
-            return None
+            self.my_logger.error(f"Error extracting description images: {e}")
 
-    def get_description_images_1(self):
         try:
-            return [img.get_attribute('src') for img in self.driver.find_elements(By.CSS_SELECTOR, 'div.description-images img')]
+            product_code_element = self.driver.find_element(By.CSS_SELECTOR, 'div.product_content_desc span.desc_col_text')
+            product_code = product_code_element.text.strip()
         except Exception as e:
-            self.my_logger.error(f"Description images 1 not found: {e}")
-            return []
+            self.my_logger.error(f"Error extracting product code: {e}")
 
-    def get_description_images_2(self):
         try:
-            return [img.get_attribute('src') for img in self.driver.find_elements(By.CSS_SELECTOR, 'div.additional-description-images img')]
+            size_element = self.driver.find_element(By.XPATH, '//div[contains(text(), "크기")]/following-sibling::div')
+            size = size_element.text.strip()
         except Exception as e:
-            self.my_logger.error(f"Description images 2 not found: {e}")
-            return []
+            self.my_logger.error(f"Error extracting size: {e}")
 
-    def get_product_code(self):
         try:
-            return self.driver.find_element(By.CSS_SELECTOR, 'span.product-code').text.strip()
+            tip_element = self.driver.find_element(By.CSS_SELECTOR, 'div.info_row div.text')
+            tip = tip_element.text.strip()
         except Exception as e:
-            self.my_logger.error(f"Product code not found: {e}")
-            return None
+            self.my_logger.error(f"Error extracting tip: {e}")
 
-    def get_size(self):
         try:
-            return self.driver.find_element(By.XPATH, '//div[contains(text(), "Size")]/following-sibling::div').text.strip()
+            thickness_element = self.driver.find_element(By.XPATH, '//div[contains(text(), "두께")]/following-sibling::div')
+            thickness = thickness_element.text.strip()
         except Exception as e:
-            self.my_logger.error(f"Size not found: {e}")
-            return None
+            self.my_logger.error(f"Error extracting thickness: {e}")
 
-    def get_tip(self):
         try:
-            return self.driver.find_element(By.CSS_SELECTOR, 'div.tip').text.strip()
+            material_element = self.driver.find_element(By.XPATH, '//div[contains(text(), "재질")]/following-sibling::div')
+            material = material_element.text.strip()
         except Exception as e:
-            self.my_logger.error(f"Tip not found: {e}")
-            return None
+            self.my_logger.error(f"Error extracting material: {e}")
 
-    def get_thickness(self):
         try:
-            return self.driver.find_element(By.XPATH, '//div[contains(text(), "Thickness")]/following-sibling::div').text.strip()
+            color_element = self.driver.find_element(By.XPATH, '//div[contains(text(), "색상")]/following-sibling::div')
+            color = color_element.text.strip()
         except Exception as e:
-            self.my_logger.error(f"Thickness not found: {e}")
-            return None
+            self.my_logger.error(f"Error extracting color: {e}")
 
-    def get_material(self):
         try:
-            return self.driver.find_element(By.XPATH, '//div[contains(text(), "Material")]/following-sibling::div').text.strip()
+            quantity_in_box_element = self.driver.find_element(By.XPATH, '//div[contains(text(), "포장단위")]/following-sibling::div')
+            quantity_in_box = quantity_in_box_element.text.strip()
         except Exception as e:
-            self.my_logger.error(f"Material not found: {e}")
-            return None
+            self.my_logger.error(f"Error extracting quantity in box: {e}")
 
-    def get_color(self):
-        try:
-            return self.driver.find_element(By.XPATH, '//div[contains(text(), "Color")]/following-sibling::div').text.strip()
-        except Exception as e:
-            self.my_logger.error(f"Color not found: {e}")
-            return None
+        self.update_excel(response.url, product_name, price, description, main_image,
+                          other_pictures, description_images, product_code, size,
+                          tip, thickness, material, color, quantity_in_box, response.meta['url_type'])
 
-    def get_quantity_in_box(self):
-        try:
-            return self.driver.find_element(By.XPATH, '//div[contains(text(), "Quantity")]/following-sibling::div').text.strip()
-        except Exception as e:
-            self.my_logger.error(f"Quantity in box not found: {e}")
-            return None
-
-    def update_excel(self, url, product_data, url_type):
+    def update_excel(self, url, product_name, price, description, main_image,
+                      other_pictures, description_images, product_code, size,
+                      tip, thickness, material, color, quantity_in_box, url_type):
         try:
             df = pd.read_excel(self.excel_file)
             if url in df['URL'].values:
                 index = df.index[df['URL'] == url].tolist()[0]
-                for key, value in product_data.items():
-                    df.at[index, key] = value if value is not None else df.at[index, key]
-                df.at[index, 'URL type'] = url_type
+                df.loc[index] = [url, product_name, price, description, main_image,
+                                 ','.join(other_pictures), ','.join(description_images), product_code, size,
+                                 tip, thickness, material, color, quantity_in_box, url_type]
             else:
-                new_row = {'URL': url, 'URL type': url_type, **product_data}
+                new_row = {
+                    'URL': url, 'product_name': product_name, 'price': price,
+                    'description': description, 'main_image': main_image,
+                    'other_pictures': ','.join(other_pictures), 'description_images': ','.join(description_images),
+                    'product_code': product_code, 'size': size, 'tip': tip,
+                    'thickness': thickness, 'material': material, 'color': color,
+                    'quantity_in_box': quantity_in_box, 'URL type': url_type
+                }
                 df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
             df.to_excel(self.excel_file, index=False)
             self.my_logger.info(f"Data saved to Excel for URL: {url}")
@@ -269,7 +216,17 @@ class ExampleSpider(scrapy.Spider):
             self.my_logger.error(f"Error updating Excel file: {e}")
 
     def is_blocked(self, response):
-        return response.status in [403, 503]
+        # Проверка на коды ошибок HTTP
+        if response.status in [403, 503]:
+            return True
+
+        # Проверка на CAPTCHA (добавьте свою логику)
+        # ...
+
+        # Проверка на пустые ответы (добавьте свою логику)
+        # ...
+
+        return False
 
     def closed(self, reason):
         self.driver.quit()
